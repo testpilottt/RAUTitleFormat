5ec67c39a46946fa242915ec699a5a4542412712
Rohan Aubeeluck <rohan.aubeeluck@gmail.com>
2023-05-21 14:58:02 +0400

    added root pom version to title

--- src/main/java/com/example/fushionplugin/SimpleFrameTitleBuilder.java
+++ src/main/java/com/example/fushionplugin/SimpleFrameTitleBuilder.java
@@ -1,9 +1,21 @@
 package com.example.fushionplugin;
 
 import com.intellij.openapi.wm.impl.PlatformFrameTitleBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 
 public class SimpleFrameTitleBuilder extends PlatformFrameTitleBuilder {
 
@@ -33,9 +45,49 @@ public class SimpleFrameTitleBuilder extends PlatformFrameTitleBuilder {
             tempVirtualFile = tempVirtualFile.getParent();
         }
 
-        title = title.replace("{DEFAULT}", "*[" + previousVirtualFile.getName() + "]* - " + super.getFileTitle(project, virtualFile));
+        String pomVersion = null;
+
+        try {
+            String pomPath = previousVirtualFile.getPath() + "/pom.xml";
+            File file = new File(pomPath);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+
+            Element element = (Element) doc.getElementsByTagName("parent").item(0);
+
+            Node projectNode = element.getParentNode();
+
+            projectNode.removeChild(element);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("project");
+
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {
+                Node node = nodeList.item(itr);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    pomVersion = eElement.getElementsByTagName("version").item(0).getTextContent();
+                }
+            }
+        } catch (Exception ignored) {
+
+        }
+
+        StringBuilder rauTitle = new StringBuilder()
+                .append("*[")
+                .append(previousVirtualFile.getName());
+
+        if (pomVersion != null) {
+            rauTitle.append(" ^ ")
+                    .append(pomVersion);
+        }
+        rauTitle.append("]* - ")
+                .append(super.getFileTitle(project, virtualFile));
+
+        title = title.replace("{DEFAULT}", rauTitle.toString());
 
         return title;
     }
-
 }
